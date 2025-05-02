db7ef2baba3e7b1bc90686de7b9a4b9df2838ff9
Luis Gomes <luis.gomes@seteinformatica.local>
2025-05-02 08:08:13 -0300

    Inicializando o projeto frontEnd E-shore.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>  
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
