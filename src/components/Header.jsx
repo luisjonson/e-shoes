aef3831e48e3977ca9b4985f731018c9b6c4a595
Jonson <luizlindonjonson@yahoo.com.br>
2025-05-04 13:32:27 -0300

    concluinso o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,59 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import logo from '../assets/logo-header.svg'
 import carrinho from '../assets/mini-cart.svg'
+import search from '../assets/Search.svg'
+
 
 const Header = () => {
 
   const HandleContainer = styled.header`
     .container{
-      background-color: var(--white);
-      padding: 20px;
-      height: 20vh;
+      padding-top: 20px;
+      padding-left: 40px;
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
       flex-direction: column;
+      background-color: var(--white);
       & .search{
       display: flex;
-      justify-content: space-around;
+      justify-content: center;
       align-items: center;
-      gap:20px;
-        &.search input{
-          width: 80%;
+      gap: 30px ;
+      position: relative;
+       & .input-wrapper {
+            position: relative;
+            width: 100%;
+            max-width: 400px;
+            display: flex;
+            justify-content: end;
+       }
+        & input{
+          width: 100%;
           height: 40px;
           border-radius: 8px;
-          opacity: 4%;
+          padding-right: 26px; /* espaço para o ícone */
+          padding-left: 10px;
+          position: relative;
+          /* opacity: 4%; */
+          &::placeholder{
+            color:var(--light-gray-2-rgba);
+            font-size: 14px;
+            font-style: italic;
+          }
         }
+
+        &.search #icon-search{
+            width: 20px;
+            right: 10px;
+            display: flex;
+            justify-content: center;
+            position: absolute; /* agora podemos posicionar no canto */
+            align-items: center;
+            pointer-events: none;
+        }
+
         &.search button{
           width: 200px;
           height: 40px;
@@ -44,7 +73,7 @@ const Header = () => {
           display: flex;
           position: relative; /* necessário para a bolinha se posicionar relativa a este container */
           align-items: center;
-          justify-content: center;
+          justify-content: flex-start;
           
           &.carrinho span{
             top: 0px;  /* ajuste conforme necessário */
@@ -65,55 +94,47 @@ const Header = () => {
       }
     }
     & .navegacao{
-      gap: 20px;
-      height: 20vh;
+      height: 15vh;
+      padding-left: 88px;
       display: flex;
-      list-style: none;
       align-items: center;
-      justify-content: space-between;
+      justify-content: flex-start;
       &.navegacao ul {
         gap: 20px;
         display:flex;
         list-style: none;
         align-items: center;
         justify-content: start;
-        & li a {
-          font-size: 16px;
-          font-weight: 500;
-          text-decoration: none;
-          color: var(--dark-gray-2);
-          &:hover {
-            color: var(--dark-gray);
-            text-decoration: underline;
-          }
-        }
       }
     }
   `
   return (
     <HandleContainer>
       <div className='container'>
-        <div className="search">
+        <div className='search'>
           <img src={logo} alt="Logo" />
-          <input type="text" placeholder="Pesquisar produto..." />
-          {/* <img src={carrinho} alt="Logo" /> */}
-
-          <Link to="cadastre">Cadastre-se</Link>
+          <div className="input-wrapper">
+            <input type="text" placeholder="Pesquisar produto..." />
+            <img id='icon-search' src={search} alt="Logo" />
+          </div>
+          <NavLink to="cadastre">Cadastre-se</NavLink>
           <button>Entrar</button>
           <div className="carrinho">
             <img src={carrinho} alt="Carrinho" />
             <span>0</span>
           </div>
         </div>
+
         <nav className='navegacao'>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/produtos">Produtos</Link></li>
-            <li><Link to="/categorias">Categorias</Link></li>
-            <li><Link to="/meusPedidos">Meus Pedidos</Link></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/produtos">Produtos</NavLink></li>
+            <li><NavLink to="/categorias">Categorias</NavLink></li>
+            <li><NavLink to="/meusPedidos">Meus Pedidos</NavLink></li>
           </ul>
         </nav>
       </div>
+
     </HandleContainer>
   )
 }
